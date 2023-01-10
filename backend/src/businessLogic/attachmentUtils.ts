a5e815eeab4de9431771eb7662a8cc835d72f466
Amutsa <tossysidney001@gmail.com>
2023-01-10 14:39:52 +0300

    Working code

--- backend/src/businessLogic/attachmentUtils.ts
+++ backend/src/businessLogic/attachmentUtils.ts
@@ -1,5 +1,5 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+const AWSXRay = require('aws-xray-sdk')
 import { createLogger } from '../utils/logger';
 
 const XAWS = AWSXRay.captureAWS(AWS)
